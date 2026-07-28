d714cad8b5e8d7c52177fb0f7bdd6726670102db
suerovr <suerovr20@gmail.com>
2026-07-28 12:30:20 +0900

    vitaminS 초기 세팅

---
+++ src/main/java/com/group3/vitamins/VitaminSApplication.java
@@ -0,0 +1,13 @@
+package com.group3.vitamins;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VitaminSApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VitaminSApplication.class, args);
+    }
+
+}
