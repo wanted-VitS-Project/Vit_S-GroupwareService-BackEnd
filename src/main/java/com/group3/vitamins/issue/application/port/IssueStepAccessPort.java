6be0914f1c5821482ea4eb8b3055d3a12f82d5a0
rladydwns2361 <gimyongjun796@gmail.com>
2026-08-06 02:25:46 +0900

    fix: 이슈 접근 포트 병합 충돌 수정

--- src/main/java/com/group3/vitamins/issue/application/port/IssueStepAccessPort.java
+++ src/main/java/com/group3/vitamins/issue/application/port/IssueStepAccessPort.java
@@ -4,6 +4,7 @@ public interface IssueStepAccessPort {
 
     StepAccessView requireAccess(Long stepId, String requesterUserId, String role);
 
+    StepAccessView requireEditable(Long stepId, String requesterUserId, String role);
 
     record StepAccessView(Long stepId, Long projectId) {
     }
